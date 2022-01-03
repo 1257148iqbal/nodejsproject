29095dbde935eb42e94102be9e549fe8588285ff
iqbal2o2o <iqbalhossain1095@gmail.com>
2022-01-04 00:47:23 +0600

    trimmed path for regular expression

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 //dependencies
 const http = require("http");
+const url = require("url");
 
 // app object - module scaffolding
 const app = {};
 
 // condiguration
 app.config = {
-    port:3000
+  port: 3000,
 };
 
 // create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, ()=>{
-      console.log(`listening to port ${app.config.port}`)
+  server.listen(app.config.port, () => {
+    console.log(`listening to port ${app.config.port}`);
   });
 };
 
 //handle request response
-app.handleReqRes=(req, res)=>{
-    res.end("Hello Word")
-}
+app.handleReqRes = (req, res) => {
+  // request hanling
+  // get the url and parse it
+  const parseUrl = url.parse(req.url, true);
+  const path=parseUrl.pathname;
+  const trimmedPath=path.replace();
+  console.log(path);
+  res.end("Hello World!!"); 
+};
 
-//start the server 
+//start the server
 app.createServer();
